cafa00637b9cfd33e6bc82a8b54d0612c945ee92
raiesbo <56681928+raiesbo@users.noreply.github.com>
2020-06-21 20:13:59 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 $("a.link_projectMenu").click(function(){
     $(".projectList").hide(500);
     $(".curriculumVitae").hide(500);
@@ -19,4 +20,26 @@ $("a.link_projectMenu").click(function(){
     $(".project").hide(500);
     $(".projectList").show(500);
   });
+*/
+var trigger = $('.navL');
+var container = $('.container');
+/*
+$(document).ready(function () {
+  // Set trigger and container variables
+    var trigger = $('.navL'), container = $('.container');
+})
 
+// Fire on click
+trigger.on('click', function(){
+  //Ser $this for re-use. Set tarfet from data attribute
+  var $this = $(this),
+    target = $this.data('target'); console.log(target)
+  // Load target page into container
+  container.load(target + ".html")
+  // Stop normal link behavior
+  return false;
+})
+*/
+AOS.init({
+  duration: 1200,
+})
